ecdd8be13004146bfb9cce8dddb7e40e635a7539
Shivam Pal <shibru127@gmail.com>
2023-11-01 02:25:21 +0530

    [ex-group-chat] remove ignoreBuilError from next.config

--- examples/group-chat/next.config.js
+++ examples/group-chat/next.config.js
@@ -6,15 +6,6 @@ const nextConfig = {
     "partyworks-server",
     "partyworks-react",
   ],
-
-  //todo remove this it's erroring with partyworks-server for some reason :/
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextConfig;
